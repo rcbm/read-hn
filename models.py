e864242e76e6a42785e6f92748b09cd4f668afcc
mattbarackman <matthew.barackman@gmail.com>
2012-03-09 20:45:15 -0800

    scrape is a go

--- models.py
+++ models.py
@@ -1,16 +1,16 @@
 from google.appengine.ext import db
 from google.appengine.api import users
 
-class node(db.Model):
+class Node(db.Model):
     id = db.IntegerProperty(required=True)
     url = db.StringProperty(required=True)
     title = db.StringProperty(required=True)
-    text = db.TextProperty(default=None)
+    #text = db.TextProperty(default=None)
     username = db.StringProperty(required=True)
     points = db.IntegerProperty(required=True)
-    type = db.IntegerProperty(required=True)
+    #type = db.IntegerProperty(required=True)
     points = db.IntegerProperty(required=True)
-    timestamp = db.DateTimeProperty(auto_now_add=False)
+    #timestamp = db.DateTimeProperty(auto_now_add=False)
     created = db.DateTimeProperty(auto_now_add=True)
     updated = db.DateTimeProperty(auto_now=True)
     commentcount = db.IntegerProperty()
