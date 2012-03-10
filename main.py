077f255ff9404ff9c0f26dae60b75760e52123fb
Roger Cosseboom <rogercosseboom@gmail.com>
2012-03-10 11:01:23 -0500

    cleaned up scrape method

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ import logging
 import datetime
 import json
 import urllib2
+import logging
 from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
@@ -40,46 +41,32 @@ class MainPage(webapp.RequestHandler):
                           'posts': posts}
 
         self.response.out.write(template.render('static/index.html', template_values))                                                                   
-                                                                       
 
-
-                                                                       
-# requires urlfetch
+        
 class Scrape(webapp.RequestHandler):
     def get(self):
         user = users.get_current_user()
-        if user:    
-            response = urllib2.urlopen('http://api.thriftdb.com/api.hnsearch.com/items/_search?&filter[fields][create_ts]=[NOW-24HOURS%20TO%20NOW]&sortby=create_ts+desc&limit=100&filter[fields][type]=submission&pretty_print=true')
+        if user:  
+            base = 'http://api.thriftdb.com/api.hnsearch.com/items/_search?'
+            url = base + '%s%s%s' % ('&filter[fields][create_ts]=[NOW-24HOURS%20TO%20NOW]',
+                                         '&limit=100',
+                                         '&filter[fields][type]=submission')
+            logging.info('URL - %s' %url)
+            response = urllib2.urlopen(url)
             content = json.loads(response.read())
             for item in content['results']:
-                i = item['item']
-                id = i['id']
-                type = i['type']
-                url = i['url']
-                domain = i['domain']
-                if url == None and domain == None:
-                    url = "http://news.ycombinator.com/item?id=%s" % id
-                    domain = "http://news.ycombinator.com/"
-                else:
-                    pass 
-                title = i['title']
-                commentcount = i['num_comments']
-                username = i['username']
-                points = i['points']
-                timestamp = i['create_ts']
-                
-                if not db.GqlQuery("SELECT * FROM Node WHERE hn_id = %s" % id).fetch(1): 
-                    scraped_content = Node( hn_id = id,
-                                            type = type,
-                                            url = url,
-                                            domain = domain,
-                                            title = title,
-                                            commentcount = commentcount,
-                                            username = username,
-                                            points = points,
-                                            #timestamp = timestamp,
-                                            #parentid = parentid,
-                                            )
+                if not db.GqlQuery("SELECT * FROM Node WHERE hn_id = %s" % item['item']['id']).get(): 
+                    i = item['item']
+                    scraped_content = Node(hn_id = i['id'],
+                                           type = i['type'],
+                                           url = i['url'] if i['url'] else "http://news.ycombinator.com/item?id=%s" % id, 
+                                           domain = i['domain'] if i['domain'] else "http://news.ycombinator.com/",
+                                           title = i['title'],
+                                           commentcount = i['num_comments'],
+                                           username = i['username'],
+                                           points = i['points'],
+                                           #timestamp = i['create_ts'],
+                                           )
                     scraped_content.put()
                 
                             
